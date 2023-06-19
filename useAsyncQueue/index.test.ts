9fa06b80b6e27369aba474f4fef6fba004c041e8
hanjiwon1 <wldnjs20725@wavve.com>
2023-06-19 09:46:32 +0900

    fix: retry 모듈 경로 수정

--- useAsyncQueue/index.test.ts
+++ useAsyncQueue/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from 'vitest'
-import { retry } from '../../.test'
+import { retry } from '../.test'
 import { useAsyncQueue } from '.'
 
 describe('useAsyncQueue', () => {
